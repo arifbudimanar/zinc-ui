5e3c6b627b3a3c52606721259b48b65651095126
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2024-12-26 17:22:40 +0700

    wip

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -8,15 +8,21 @@ class InstallCommand extends Command
 {
     public $signature = 'zinc:install';
 
-    public $description = 'Install the Zinc UI component.';
+    public $description = 'Install the Zinc UI components';
 
     public function handle(): int
     {
-        $this->comment('Install Zinc UI ...');
-        $this->comment('Install Livewire');
-        $this->comment('Install Tailwind CSS');
+        $this->comment('Checking is Livewire installed');
+        // Make sure Livewire is installed
+        // Make sure app.js is configured
+        // If not, configute to use livewire and alpine from esm module
+        $this->comment('Checking Tailwind CSS is installed');
+        // Make sure Tailwind CSS is installed
         $this->comment('Install Livewire Toaster');
+        // Install Livewire Toaster
         $this->comment('Install Alpine Autozise');
+        // Install Alpine Autozise
+        $this->comment('Zinc UI installed successfully');
 
         return self::SUCCESS;
     }
