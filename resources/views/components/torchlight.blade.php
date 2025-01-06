60ee71670ee7fdac76011a99208fe5c2b2b23378
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-01-06 14:14:02 +0700

    wip alpha 11

---
+++ resources/views/components/torchlight.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'language' => 'blade',
+    'lightTheme' => 'github-light',
+    'darkTheme' => 'material-theme-ocean',
+    'content' => null,
+])
+
+<div class="min-w-max">
+    <x-torchlight-code :$language :theme="$lightTheme" :$content
+        {{ $attributes->merge(['class' => 'block min-w-full dark:hidden !bg-transparent whitespace-pre text-base p-6']) }}>
+        {{ $slot }}
+    </x-torchlight-code>
+    <x-torchlight-code :$language :theme="$darkTheme" :$content x-ref="copyCode"
+        {{ $attributes->merge(['class' => 'hidden min-w-full dark:block !bg-transparent whitespace-pre text-base p-6']) }}>
+        {{ $slot }}
+    </x-torchlight-code>
+</div>
