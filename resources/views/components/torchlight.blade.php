1262bf60af682995243291ef2299a01c812107a3
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-01-10 05:02:40 +0700

    wip alpha 23

--- resources/views/components/torchlight.blade.php
+++ resources/views/components/torchlight.blade.php
@@ -6,11 +6,11 @@
 ])
 
 <div class="min-w-max">
-    <x-torchlight-code :$language :theme="$lightTheme" :$content
+    <x-torchlight-code :$language :theme="$lightTheme" :$content data-torchlight
         {{ $attributes->merge(['class' => 'block min-w-full dark:hidden !bg-transparent whitespace-pre text-base p-6']) }}>
         {{ $slot }}
     </x-torchlight-code>
-    <x-torchlight-code :$language :theme="$darkTheme" :$content x-ref="copyCode"
+    <x-torchlight-code :$language :theme="$darkTheme" :$content x-ref="copyCode" data-torchlight
         {{ $attributes->merge(['class' => 'hidden min-w-full dark:block !bg-transparent whitespace-pre text-base p-6']) }}>
         {{ $slot }}
     </x-torchlight-code>
