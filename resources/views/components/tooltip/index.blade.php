419b1a2023cd955e017758c4c619cccb04a2a2a2
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-01-14 12:54:01 +0700

    fix tooltip toggleable

--- resources/views/components/tooltip/index.blade.php
+++ resources/views/components/tooltip/index.blade.php
@@ -14,43 +14,17 @@
 @if ($toggleable)
     <div {{ $attributes->merge(['id' => $id]) }} x-data="{
         isTooltipOpen: false,
-        touchTimeout: null,
-        isTouchDevice: false,
         openTooltip() {
-            if (!this.isTouchDevice) {
-                this.isTooltipOpen = true;
-            }
+            this.isTooltipOpen = true;
         },
         toggleTooltip() {
-            if (!this.isTouchDevice) {
-                this.isTooltipOpen = !this.isTooltipOpen;
-            }
+            this.isTooltipOpen = !this.isTooltipOpen;
         },
         closeTooltip() {
             this.isTooltipOpen = false;
-            if (this.touchTimeout) {
-                clearTimeout(this.touchTimeout);
-                this.touchTimeout = null;
-            }
         },
-        handleTouchStart(e) {
-            this.isTouchDevice = true;
-            this.touchTimeout = setTimeout(() => {
-                this.isTooltipOpen = true;
-            }, 500);
-        },
-        handleTouchEnd(e) {
-            if (this.touchTimeout) {
-                clearTimeout(this.touchTimeout);
-                this.touchTimeout = null;
-            }
-        },
-        init() {
-            this.isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-        }
-    }" x-ref="tooltip" x-init="init()"
-        x-on:click="toggleTooltip" x-on:click.outside="closeTooltip" x-on:touchstart="handleTouchStart"
-        x-on:touchend="handleTouchEnd" x-on:touchcancel="closeTooltip" x-on:touchmove="closeTooltip" data-tooltip>
+    }" x-ref="tooltip" x-on:click="toggleTooltip"
+        x-on:click.outside="closeTooltip" data-tooltip>
 
         {{ $slot }}
 
@@ -96,10 +70,10 @@
             this.isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
         }
     }" x-ref="tooltip" x-init="init()"
-        x-on:click="closeTooltip" x-on:click.outside="closeTooltip" x-on:focusin="openTooltip"
-        x-on:focusout="closeTooltip" x-on:mouseenter="openTooltip" x-on:mouseleave="closeTooltip"
-        x-on:keydown.esc="closeTooltip" x-on:touchstart="handleTouchStart" x-on:touchend="handleTouchEnd"
-        x-on:touchcancel="closeTooltip" x-on:touchmove="closeTooltip" data-tooltip>
+        x-on:click="closeTooltip" x-on:click.outside="closeTooltip" x-on:focusin="openTooltip" x-on:focusout="closeTooltip"
+        x-on:mouseenter="openTooltip" x-on:mouseleave="closeTooltip" x-on:keydown.esc="closeTooltip"
+        x-on:touchstart="handleTouchStart" x-on:touchend="handleTouchEnd" x-on:touchcancel="closeTooltip"
+        x-on:touchmove="closeTooltip" data-tooltip>
 
         {{ $slot }}
 
