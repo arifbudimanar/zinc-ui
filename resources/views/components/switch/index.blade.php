f6fbba2e6699cd83e09da6cbf4d46af11234eed5
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-01-26 01:46:34 +0700

    wip

--- resources/views/components/switch/index.blade.php
+++ resources/views/components/switch/index.blade.php
@@ -50,12 +50,7 @@
             ]) }}
             data-control />
 
-        <div class="peer w-8 h-5 rounded-full appearance-none shadow-sm disabled:shadow-none dark:shadow-none
-        bg-zinc-300 dark:bg-transparent peer-checked:bg-black dark:peer-checked:bg-white peer-disabled:opacity-50
-        dark:border border-zinc-300 dark:border-zinc-600 peer-checked:border-0
-        after:absolute after:content-[''] after:top-[3px] after:left-[3px] after:h-3.5 after:w-3.5
-        after:rounded-full after:transition-all peer-checked:after:translate-x-3
-        after:bg-white dark:after:peer-checked:bg-zinc-800 cursor-pointer peer-disabled:cursor-default focus:outline-1"
+        <div class="peer w-8 h-5 rounded-full appearance-none shadow-sm disabled:shadow-none dark:shadow-none bg-zinc-800/15 dark:bg-transparent peer-checked:bg-black dark:peer-checked:bg-white peer-disabled:opacity-50 dark:border border-zinc-300 dark:border-white/20 peer-checked:border-0 after:absolute peer-disabled:cursor-default focus:outline-1"
             x-on:click.prevent="$el.previousElementSibling.click()"
             x-on:keydown.enter.prevent="$el.previousElementSibling.click()"
             x-on:keydown.space.prevent="$el.previousElementSibling.click()"
@@ -64,7 +59,9 @@
             :class="{
                 'cursor-pointer': !$el.previousElementSibling.disabled,
                 'cursor-default': $el.previousElementSibling.disabled,
-            }"
+            }">
+        </div>
+        <div class="absolute bg-white peer-checked:bg-white dark:peer-checked:bg-zinc-800 pointer-events-none top-[3px] left-[3px] size-3.5 rounded-full transition-all peer-checked:translate-x-3"
             data-switch-indicator>
         </div>
     </label>
