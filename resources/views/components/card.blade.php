10dad9f869a7eda6e91d0b5f0b94d6bbc10ab594
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-02-01 02:00:03 +0700

    wip

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,3 @@
-<div {{ $attributes->merge(['class' => 'w-full p-6 border rounded-xl bg-white dark:bg-white/10 border border-zinc-200 dark:border-white/10']) }}
-    data-card>
+<div {{ $attributes->class('w-full p-6 border rounded-xl bg-white dark:bg-white/10 border border-zinc-200 dark:border-white/10') }} data-card>
     {{ $slot }}
 </div>
