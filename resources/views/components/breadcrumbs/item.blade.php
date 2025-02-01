3fc66c87369b88f333fdfaec8bf4d2d029d0b67b
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-02-01 09:40:35 +0700

    wip

--- resources/views/components/breadcrumbs/item.blade.php
+++ resources/views/components/breadcrumbs/item.blade.php
@@ -8,7 +8,7 @@
 
 <div class="group flex items-center text-sm font-medium [&_[data-dropdown]]:-mx-1.5" data-breadcrumbs-item>
     <?php if ($href): ?>
-        <a {{ $attributes->class('text-zinc-800 dark:text-white hover:underline decoration-zinc-800/20 underline-offset-4') }}>
+        <a href={{ $href }} {{ $attributes->class('text-zinc-800 dark:text-white hover:underline decoration-zinc-800/20 underline-offset-4') }}>
             <?php if ($icon): ?>
                 <x-icon name="{{ $icon }}" class="size-5 shrink-0" />
             <?php else: ?>
@@ -26,10 +26,8 @@
     <?php endif; ?>
 
     <?php if ($separator === 'chevron-right'): ?>
-        <x-icon name="o-chevron-right"
-            class="mx-2 size-4 shrink-0 text-zinc-300 group-last:hidden dark:text-white/80" />
+        <x-icon name="o-chevron-right" :attributes="$attributes->class('mx-2 size-5 shrink-0 text-zinc-300 group-last:hidden dark:text-white/80')" />
     <?php elseif ($separator === 'slash'): ?>
-        <x-icon name="o-slash"
-            class="mx-2 size-4 shrink-0 text-zinc-300 group-last:hidden dark:text-white/80" />
+        <x-icon name="o-slash" :attributes="$attributes->class('mx-2 size-5 shrink-0 text-zinc-300 group-last:hidden dark:text-white/80')" />
     <?php endif; ?>
 </div>
