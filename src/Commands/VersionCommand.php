3dfd2277a72fe3d0a6fda1a9931f79d13b5ae612
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-02-01 21:32:52 +0700

    wip alpha 67

--- src/Commands/VersionCommand.php
+++ src/Commands/VersionCommand.php
@@ -12,7 +12,7 @@ class VersionCommand extends Command
 
     public function handle(): int
     {
-        $this->comment('Zinc UI v0.1-alpha.66');
+        $this->comment('Zinc UI v0.1-alpha.67');
 
         return self::SUCCESS;
     }
