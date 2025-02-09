407f08887bcadbb57caf0a5e460659ae906f1ca2
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-02-09 22:55:53 +0700

    wip

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -5,7 +5,7 @@
 @php
     $classes = ZincUi::classes()
         ->add('[grid-area:main] p-6 lg:p-8 w-full')
-        ->add($container ? 'mx-auto' : '');
+        ->add($container ? 'mx-auto max-w-7xl' : '');
 @endphp
 
 <main {{ $attributes->class($classes) }} data-main>
