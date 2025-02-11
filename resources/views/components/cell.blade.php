483011943dbc6f54237831d198173173d182aa83
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-02-11 13:31:33 +0700

    wip

--- resources/views/components/cell.blade.php
+++ resources/views/components/cell.blade.php
@@ -6,8 +6,8 @@
     $classes = ZincUi::classes()
         ->add(
             match ($variant) {
-                'default' => 'text-sm',
-                'strong' => 'text-medium',
+                'default' => 'text-sm font-normal',
+                'strong' => 'text-sm font-medium',
             },
         )
         ->add('py-3 px-3 first:pl-0 last:pr-0 text-zinc-500 dark:text-zinc-300');
