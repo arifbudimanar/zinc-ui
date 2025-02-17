a96bc0e5f36e28f656275e397beb33e9fc1f6aa1
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-02-17 18:00:58 +0700

    wip

--- resources/views/components/options.blade.php
+++ resources/views/components/options.blade.php
@@ -8,8 +8,15 @@
     'offset' => $offset,
 ])
 
-<div
-    {{ $attributes->class('z-20 [:where(&)]:min-w-48 w-full [:where(&)]:max-h-[20rem] p-[.3125rem] rounded-lg shadow-sm border border-zinc-200 dark:border-zinc-600 bg-white dark:bg-zinc-700 overflow-y-auto') }}
+@php
+    $classes = ZincUi::classes()
+        ->add('z-20 p-[.3125rem] rounded-lg shadow-sm overflow-y-auto')
+        ->add('border border-zinc-200 dark:border-zinc-600')
+        ->add('bg-white dark:bg-zinc-700')
+        ->add('[:where(&)]:min-w-48 w-full [:where(&)]:max-h-[20rem]');
+@endphp
+
+<div {{ $attributes->class($classes) }}
     x-show="isSelectOpen" x-cloak
     x-anchor.{{ $position }}.offset.{{ $offset }}="$refs.select"
     x-trap.noautofocus="isSelectOpen"
